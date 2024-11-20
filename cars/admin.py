c423a2eaafdbde43aab254c00a50a5478f0834ea
cr <cswd@yandex.ru>
2024-11-21 04:04:48 +0700

    Registered Car and Comment models.

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Car, Comment
 
-# Register your models here.
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('make', 'model', 'year', 'owner', 'created_at')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('content', 'car', 'author', 'created_at')
