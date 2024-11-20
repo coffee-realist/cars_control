04cc88959b15c618484355c2d20fe59e44f0f7c4
cr <cswd@yandex.ru>
2024-11-21 04:10:46 +0700

    Implemented comments handling in CarViewSet with GET and POST methods.

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+from .models import Car
+from .serializers import CarSerializer, CommentSerializer
 
-# Create your views here.
+
+class CarViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'comments' and self.request.method == 'POST':
+            return CommentSerializer
+        return super().get_serializer_class()
+
+    @action(detail=True, methods=['get', 'post'], url_path='comments')
+    def comments(self, request, pk=None):
+        car = self.get_object()
+
+        if request.method == 'GET':
+            comments = car.comments.all()
+            serializer = CommentSerializer(comments, many=True)
+            return Response(serializer.data)
+
+        elif request.method == 'POST':
+            if not request.user.is_authenticated:
+                return Response(
+                    {"detail": "Authentication credentials were not provided."},
+                    status=status.HTTP_401_UNAUTHORIZED
+                )
+
+            serializer = CommentSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save(car=car, author=request.user)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
