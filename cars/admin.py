eddc4da2ae7f66f066bb9ae9da99038d0b0a73b0
cr <cswd@yandex.ru>
2024-11-22 00:36:59 +0700

    Upgraded Admin DRF panel.

--- cars/admin.py
+++ cars/admin.py
@@ -4,9 +4,19 @@ from .models import Car, Comment
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('make', 'model', 'year', 'owner', 'created_at')
+    list_display = ('make', 'model', 'year', 'owner', 'created_at', 'updated_at')
+    list_filter = ('year', 'make', 'owner')
+    search_fields = ('make', 'model', 'owner__username')
+    ordering = ('-created_at', 'make',)
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('content', 'car', 'author', 'created_at')
+    list_display = ('content', 'car_link', 'author', 'created_at')
+    list_filter = ('car__make', 'author')
+    search_fields = ('content', 'author__username', 'car__make')
+    ordering = ('-created_at',)
+
+    def car_link(self, obj):
+        return admin.utils.format_html('<a href="{}">{}</a>', obj.car.get_absolute_url(), obj.car)
+    car_link.short_description = "Car"
