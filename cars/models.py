76013401ab8d47c647c9971c902dd7b4f898fd05
cr <cswd@yandex.ru>
2024-11-22 00:37:28 +0700

    Added validation.

--- cars/models.py
+++ cars/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.timezone import now
 
 
 class Car(models.Model):
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
-    year = models.IntegerField()
+    year = models.IntegerField(
+        validators=[
+            MinValueValidator(1886),
+            MaxValueValidator(now().year)
+        ]
+    )
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
