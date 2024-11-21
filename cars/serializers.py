9de2161f0459b7d2fc62c52d654741f96b174373
cr <cswd@yandex.ru>
2024-11-22 00:39:17 +0700

    Changed fields presentation of Cars.

--- cars/serializers.py
+++ cars/serializers.py
@@ -1,11 +1,25 @@
 from rest_framework import serializers
 from .models import Car, Comment
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
 
 
 class CarSerializer(serializers.ModelSerializer):
+    owner = serializers.SerializerMethodField()
+
     class Meta:
         model = Car
-        fields = '__all__'
+        fields = ['id', 'make', 'model', 'year', 'description', 'created_at', 'updated_at', 'owner']
+        read_only_fields = ['created_at', 'updated_at', 'owner', 'id']
+
+    @staticmethod
+    def get_owner(obj):
+        return obj.owner.username
 
 
 class CommentSerializer(serializers.ModelSerializer):
