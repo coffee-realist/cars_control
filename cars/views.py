2519da5fbfe5eab756cf0cd90b4eca92a1542784
cr <cswd@yandex.ru>
2024-11-23 03:06:41 +0700

    Added comment serializer getter. Removed registration. Added View for car deleting.

--- cars/views.py
+++ cars/views.py
@@ -2,13 +2,10 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
-
-from .forms import RegistrationForm
-from .models import Car
+from django.views.generic import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .serializers import CarSerializer, CommentSerializer
+from .models import Car
 from .permissions import IsOwnerOrReadOnly
 
 
@@ -20,12 +17,7 @@ class CarViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-    def get_serializer_class(self):
-        if self.action == 'comments' and self.request.method == 'POST':
-            return CommentSerializer
-        return super().get_serializer_class()
-
-    @action(detail=True, methods=['get', 'post'], url_path='comments')
+    @action(detail=True, methods=['get', 'post'], url_path='comments', permission_classes=[IsAuthenticated])
     def comments(self, request, pk=None):
         car = self.get_object()
 
@@ -35,28 +27,22 @@ class CarViewSet(viewsets.ModelViewSet):
             return Response(serializer.data)
 
         elif request.method == 'POST':
-            if not request.user.is_authenticated:
-                return Response(
-                    {"detail": "Authentication credentials were not provided."},
-                    status=status.HTTP_401_UNAUTHORIZED
-                )
-
             serializer = CommentSerializer(data=request.data)
             if serializer.is_valid():
                 serializer.save(car=car, author=request.user)
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def get_serializer_class(self):
+        if self.action == 'comments':
+            return CommentSerializer
+        return super().get_serializer_class()
 
-def register(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            login(request, user)
-            return redirect('car-list')
-    else:
-        form = RegistrationForm()
-    return render(request, 'cars/register.html', {'form': form})
+
+class CarDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Car
+    success_url = '/profile'
+
+    def test_func(self):
+        car = self.get_object()
+        return car.owner == self.request.user
