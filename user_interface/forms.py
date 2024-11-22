0ea86a0d3a32354f41d802b7e85979e3e815e796
cr <cswd@yandex.ru>
2024-11-23 03:13:34 +0700

    Added RegistrationForm to user interface.

---
+++ user_interface/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password_confirm = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+                                       label="Подтверждение пароля")
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+
+        if password != password_confirm:
+            self.add_error('password_confirm', "Пароли не совпадают.")
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
